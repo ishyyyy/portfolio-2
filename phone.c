07a4f963fe7495a6ce8293bea68f14751b1c5158
Isha Sharma <86165115+ishyyyy@users.noreply.github.com>
2022-10-15 00:13:31 +0530

    Add files via upload

---
+++ phone.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("ENTER PHONE NUMBER \n:");
+    printf("6306872317\n");
+    printf("REALME8(163..) never in your Range\n ");
+    printf("distance Between entered number and 868098059397930 is 0 m\n");
+
+
+}
